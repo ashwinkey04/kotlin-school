4419f7e7ee295d01d940183a9e9061d044540049
Ashwin R <ashwinkey04@gmail.com>
2019-11-03 13:59:35 +0530

    Add clear button

--- android/DiceRoller/app/src/main/java/tk/imagingens/diceroller/MainActivity.kt
+++ android/DiceRoller/app/src/main/java/tk/imagingens/diceroller/MainActivity.kt
@@ -2,11 +2,13 @@ package tk.imagingens.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,15 +18,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        val clearButton: Button = findViewById(R.id.clear_button)
+
         rollButton.setOnClickListener() {
             Toast.makeText(this, "Rolled!", Toast.LENGTH_SHORT).show()
             rollDice()
             rollButton.text = getString(R.string.RollIt)
         }
+        clearButton.setOnClickListener(){
+            clearDice()
+        }
         diceImage = findViewById(R.id.dice_image)
     }
-
+    private fun clearDice(){
+        diceImage.setImageResource(R.drawable.empty_dice)
+    }
     private fun rollDice() {
+        clear_button.visibility = View.VISIBLE
         val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
