b273ddb24a45456ebb3f9e4874d6cfba2b7ec399
Ashwin R <ashwinkey04@gmail.com>
2019-11-03 17:19:57 +0530

    Update AboutMe

---
+++ android/AboutMe/README.md
@@ -0,0 +1,5 @@
+# About Me
+This app uses nested views (ScrollView inside LinearLayout). The scroll view displays a normal paragraph. Uses Apple's SF Pro for all the textView elements.
+
+<img src="screen.png" width="210">
+
--- android/AboutMe/app/src/main/java/tk/imagingens/aboutme/MainActivity.kt
+++ android/AboutMe/app/src/main/java/tk/imagingens/aboutme/MainActivity.kt
@@ -1,12 +1,36 @@
 package tk.imagingens.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_done).setOnClickListener {
+            addNickname(it)
+        }
     }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
